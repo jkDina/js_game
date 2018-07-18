a30fe03d2039dc93fb869d161cb151fa6237c5b6
jkDina <jkrhj@mail.ru>
2018-07-18 20:04:04 +0100

    Second commit

--- game.js
+++ game.js
@@ -1,7 +1,4 @@
 'use strict';
-/*Необходимо реализовать класс Vector, который позволит
-контролировать расположение объектов в двумерном пространстве и
-управлять их размером и перемещением.*/
 class Vector {
     constructor(x = 0, y = 0) {
         this.x = x;
@@ -17,15 +14,6 @@ class Vector {
         return new Vector(this.x * k, this.y * k);
     }
 }
-
-/*
-const start = new Vector(30, 50);
-const moveTo = new Vector(5, 10);
-const finish = start.plus(moveTo.times(2));
-
-console.log(`Исходное расположение: ${start.x}:${start.y}`);
-console.log(`Текущее расположение: ${finish.x}:${finish.y}`);
-*/
 class Actor {
     constructor(pos = new Vector(0,0), size = new Vector(1,1), speed = new Vector(0,0)) {
         if (!(pos instanceof Vector)) {
@@ -65,9 +53,6 @@ class Actor {
         }
         if (actor === this)
             return false;
-        /*если правая граница 1-го прямоугольника лежит между левой и
-        правой границей 2-го прямоугольника или наоборот (правая  граница 2- го прямоугольника лежит
-        между правой и левой границей  1-го прямоугольника*/
         const condX = (this.right <= actor.right) && (this.right >= actor.left) || (actor.right <= this.right) && (actor.right >= this.left);
         const condY = (this.top >= actor.top) && (this.top <= actor.bottom) || (actor.top >= this.top) && (actor.top <= this.bottom);
 
@@ -81,36 +66,6 @@ class Actor {
         return false;
     }
 }
-/*
-const items = new Map();
-const player = new Actor();
-items.set('Игрок', player);
-items.set('Первая монета', new Actor(new Vector(10, 10)));
-items.set('Вторая монета', new Actor(new Vector(15, 5)));
-
-function position(item) {
-  return ['left', 'top', 'right', 'bottom']
-    .map(side => `${side}: ${item[side]}`)
-    .join(', ');
-}
-
-function movePlayer(x, y) {
-  player.pos = player.pos.plus(new Vector(x, y));
-}
-
-function status(item, title) {
-  console.log(`${title}: ${position(item)}`);
-  if (player.isIntersect(item)) {
-    console.log(`Игрок подобрал ${title}`);
-  }
-}
-
-items.forEach(status);
-movePlayer(10, 10);
-items.forEach(status);
-movePlayer(5, -5);
-items.forEach(status);
-*/
 class Level {
     constructor(grid, actors) {
         if (!actors) actors = [];
@@ -199,45 +154,6 @@ class Level {
         }
     }
 }
-/*
-const grid = [
-  [undefined, undefined],
-  ['wall', 'wall']
-];
-
-function MyCoin(title) {
-  this.type = 'coin';
-  this.title = title;
-}
-MyCoin.prototype = Object.create(Actor);
-MyCoin.constructor = MyCoin;
-
-const goldCoin = new MyCoin('Золото');
-const bronzeCoin = new MyCoin('Бронза');
-const player = new Actor();
-const fireball = new Actor();
-
-const level = new Level(grid, [ goldCoin, bronzeCoin, player, fireball ]);
-
-level.playerTouched('coin', goldCoin);
-level.playerTouched('coin', bronzeCoin);
-
-if (level.noMoreActors('coin')) {
-  console.log('Все монеты собраны');
-  console.log(`Статус игры: ${level.status}`);
-}
-
-const obstacle = level.obstacleAt(new Vector(1, 1), player.size);
-if (obstacle) {
-  console.log(`На пути препятствие: ${obstacle}`);
-}
-
-const otherActor = level.actorAt(player);
-if (otherActor === fireball) {
-  console.log('Пользователь столкнулся с шаровой молнией');
-}
-*/
-
 class LevelParser {
     constructor(actorsDict) {
         this.actorsDict = actorsDict;
@@ -301,13 +217,11 @@ class Fireball extends Actor {
         return 'fireball';
     }
     getNextPosition(t = 1) {
-        const x = this.pos.x + this.speed.x * t;
-        const y = this.pos.y + this.speed.y * t;
-        return new Vector(x,y);
+        const v = this.speed.times(t).plus(this.pos);
+        return v;
     }
     handleObstacle() {
-        this.speed.x = -this.speed.x;
-        this.speed.y = -this.speed.y;
+        this.speed = this.speed.times(-1);
     }
     act(t, level) {
         const pos = this.getNextPosition(t);
@@ -341,7 +255,7 @@ class FireRain extends Fireball {
 class Coin extends Actor {
     constructor(pos) {
         pos = pos || new Vector(0, 0);
-        const startPos = new Vector(pos.x + 0.2, pos.y + 0.1);
+        const startPos = pos.plus(new Vector(0.2, 0.1));
         super(startPos, new Vector(0.6,0.6));
         this.startPos = startPos;
         this.springSpeed = 8;
@@ -360,7 +274,7 @@ class Coin extends Actor {
     getNextPosition(t = 1) {
         this.updateSpring(t);
         const springVector = this.getSpringVector();
-        return new Vector(this.startPos.x + springVector.x, this.startPos.y + springVector.y);
+        return this.startPos.plus(springVector);
     }
     act(t = 1) {
         this.pos = this.getNextPosition(t);
@@ -375,59 +289,3 @@ class Player extends Actor {
         return 'player';
     }
 }
-/*
-const time = 5;
-const speed = new Vector(1, 0);
-const position = new Vector(5, 5);
-
-const ball = new Fireball(position, speed);
-
-const nextPosition = ball.getNextPosition(time);
-console.log(`Новая позиция: ${nextPosition.x}: ${nextPosition.y}`);
-
-ball.handleObstacle();
-console.log(`Текущая скорость: ${ball.speed.x}: ${ball.speed.y}`);
-*/
-
-/*
-const plan = [
-  ' @ ',
-  'x!x'
-];
-
-const actorsDict = Object.create(null);
-actorsDict['@'] = Actor;
-
-const parser = new LevelParser(actorsDict);
-const level = parser.parse(plan);
-
-level.grid.forEach((line, y) => {
-  line.forEach((cell, x) => console.log(`(${x}:${y}) ${cell}`));
-});
-
-level.actors.forEach(actor => console.log(`(${actor.pos.x}:${actor.pos.y}) ${actor.type}`));
-*/
-
-/*const grid = [
-  new Array(3),
-  ['wall', 'wall', 'lava']
-];
-//const level = new Level(grid);
-//runLevel(level, DOMDisplay);
-
-const schema = [
-  '         ',
-  '         ',
-  '         ',
-  '         ',
-  '     !xxx',
-  ' @       ',
-  'xxx!     ',
-  '         '
-];
-const actorDict = {
-  '@': Player
-}
-const parser = new LevelParser(actorDict);
-const level = parser.parse(schema);
-runLevel(level, DOMDisplay);*/
